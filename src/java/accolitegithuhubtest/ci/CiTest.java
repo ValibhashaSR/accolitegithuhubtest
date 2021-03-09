529870d178a279a35bb896b343f1a23d34e358fa
ValibhashaSR <valibhasha.rasheedsab@accolitedigital.com>
2021-03-09 17:09:40 +0530

    formate

--- src/java/accolitegithuhubtest/ci/CiTest.java
+++ src/java/accolitegithuhubtest/ci/CiTest.java
@@ -1,7 +1,8 @@
 package accolitegithuhubtest.ci;
 
 public class CiTest {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("CI Test");
 }
 }
