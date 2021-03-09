3d8eabea518866ccb8c3ace22e08f3696e3e270d
ValibhashaSR <valibhasha.rasheedsab@accolitedigital.com>
2021-03-09 17:10:55 +0530

    format

--- src/java/accolitegithuhubtest/newfeatures/SecondFeatureAdd.java
+++ src/java/accolitegithuhubtest/newfeatures/SecondFeatureAdd.java
@@ -1,7 +1,8 @@
 package accolitegithuhubtest.newfeatures;
 
 public class SecondFeatureAdd {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("second feaures added");
 	System.out.println(".class file missed issue");
 }
