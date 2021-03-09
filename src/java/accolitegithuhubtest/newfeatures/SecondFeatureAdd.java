529099e8b24a078903d96e6757bddde65a5b58e7
ValibhashaSR <valibhasha.rasheedsab@accolitedigital.com>
2021-03-09 19:23:28 +0530

    environment test

--- src/java/accolitegithuhubtest/newfeatures/SecondFeatureAdd.java
+++ src/java/accolitegithuhubtest/newfeatures/SecondFeatureAdd.java
@@ -3,6 +3,7 @@ package accolitegithuhubtest.newfeatures;
 public class SecondFeatureAdd {
 public static void main(String[] args) 
 {
+	System.out.println("test env");
 	System.out.println("second feaures added");
 }
 }
